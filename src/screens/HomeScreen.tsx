1a9895fea3c7a9b221eddfe7b1c8de55859fa770
Miguel Angel Lupani <mlupani2@gmail.com>
2021-07-15 17:17:49 -0300

    primer commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+            <Text>Hola mundo</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
